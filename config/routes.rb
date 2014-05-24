46c4a0edf31dadf504a5081b107968b731fab122
Thuong Ho <thuong.t.ho@gmail.com>
2014-05-23 21:02:32 -0700

    add sign in and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :goals
+  devise_for :users do
+    resources :goals
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
