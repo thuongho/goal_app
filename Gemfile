3d28c5c925eaaf952c1eca475640e5976e5a1209
Thuong Ho <thuong.t.ho@gmail.com>
2014-05-30 23:02:18 -0700

    finish login with twitter

--- Gemfile
+++ Gemfile
@@ -19,6 +19,11 @@ gem 'coffee-rails', '~> 4.0.0'
 # Devise for User authentication
 gem 'devise'
 
+# Omniauth for login through remote sites like facebook or twitter
+gem 'omniauth'
+gem 'omniauth-twitter'
+gem 'omniauth-facebook'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140530211633) do
+ActiveRecord::Schema.define(version: 20140531042631) do
 
   create_table "goals", force: true do |t|
     t.string   "name"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20140530211633) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "providers"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
